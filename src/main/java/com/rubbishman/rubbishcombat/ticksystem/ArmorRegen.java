76696f5451fadf135dd0b392f66cc5ca0d972a9d
bartlejona <jonathan@kitomba.com>
2020-05-11 12:38:26 +1200

    Use ticksystem..?

---
+++ src/main/java/com/rubbishman/rubbishcombat/ticksystem/ArmorRegen.java
@@ -0,0 +1,14 @@
+package com.rubbishman.rubbishcombat.ticksystem;
+
+import com.rubbishman.rubbishRedux.experimental.actionTrack.ActionTrack;
+import com.rubbishman.rubbishRedux.experimental.actionTrack.TickSystem;
+
+public class ArmorRegen extends TickSystem {
+    public void beforeDispatchStarted(ActionTrack actionQueue, Long nowTime) {
+
+    }
+
+    public void afterDispatchFinished() {
+
+    }
+}
